4d9ece69b86f221de3586d3cd21c1bfdc0fe91fa
Pix3lAssassin <th3koolperson@gmail.com>
2021-05-13 22:57:05 -0600

    added newSonnet action

--- front-end/src/actions/changeSonnet.js
+++ front-end/src/actions/changeSonnet.js
@@ -1,5 +1,15 @@
-var changeSonnet = (sonnet) => ({
+export var changeSonnet = (sonnet) => ({
   //TODO:  Return object with type and string for sonnet
+  type: 'CHANGE_SONNET',
+  sonnet
 });
 
-export default changeSonnet;
+export var newSonnet = () => ({
+  type: 'NEW_SONNET'
+});
+
+
+
+
+
+
