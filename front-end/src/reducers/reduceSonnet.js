5fb429fc60e83ebaed90b318f309c41a5a921a8e
Pix3lAssassin <th3koolperson@gmail.com>
2021-05-13 23:11:10 -0600

    added case for new sonnet

--- front-end/src/reducers/reduceSonnet.js
+++ front-end/src/reducers/reduceSonnet.js
@@ -5,6 +5,8 @@ var reduceSonnet = (state = '', action) => {
   //TODO: define a reducer for the sonnet field of our state.
   switch (action.type) {
   case 'CHANGE_SONNET':
+    return action.sonnet;
+  case 'NEW_SONNET':
     return generateSonnet();
   default:
     return state;
